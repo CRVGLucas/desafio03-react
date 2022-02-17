22e40286a679af8df2f918b917fd06875a445271
Lucas <lucas@techdog.com.br>
2022-02-17 16:30:03 -0300

    ajustes no header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,8 +22,8 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+   const [products, setProducts] = useState<ProductFormatted[]>([]);
+   const { addProduct, cart } = useCart();
 
   // const cartItemsAmount = cart.reduce((sumAmount, product) => {
   //   // TODO
